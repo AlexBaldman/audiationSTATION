3bdd2478a885bfea1b55eab1d05ef38a2bd3379d
Alex Beckerman <alexlbeckerman@gmail.com>
2025-11-23 22:07:03 -0300

    Updates

---
+++ assets/index-BBAYRrNx.js
@@ -0,0 +1,1 @@
+import"./Navigation-Ct1b79JU.js";import"./main-BlO-8d4U.js";import"./index-CiSUTyIo.js";import"./AppInitializer-anmfjxeg.js";
---
+++ assets/index-CiSUTyIo.js
@@ -0,0 +1,1 @@
+import{A as i}from"./AppInitializer-anmfjxeg.js";import{N as n}from"./Navigation-Ct1b79JU.js";document.addEventListener("DOMContentLoaded",()=>{new i().init(),new n});
